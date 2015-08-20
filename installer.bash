e818d8e9168f52d18ac508627484ef59f0a8741a
omochimetaru <omochi.metaru@gmail.com>
2015-08-20 17:10:01 +0900

    fix installer small bug

--- installer.bash
+++ installer.bash
@@ -4,9 +4,11 @@ set -ueo pipefail
 install_path="/usr/local/lib/bsr"
 bin_path="/usr/local/bin/bsr"
 
+install_parent_dir="$(dirname "$install_path")"
 install_dir_name="$(basename "$install_path")"
 
-cd "$(dirname "$install_path")"
+mkdir -p "$install_parent_dir"
+cd "$install_parent_dir"
 git clone "https://github.com/omochi/bsr.git" "$install_dir_name"
 cd "$install_dir_name"
 
